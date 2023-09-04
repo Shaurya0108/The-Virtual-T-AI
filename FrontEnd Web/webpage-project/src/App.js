ba62509ecee2f8b2a90cfe18ef3ab6a25a3f7177
Shreyon Roy <shreyon312@gmail.com>
2023-09-04 14:53:59 -0500

    First commit MVP

---
+++ FrontEnd Web/webpage-project/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello World
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
