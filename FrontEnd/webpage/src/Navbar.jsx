a5aceafb44f685c6e020b8a5d63650ac0f13a3f0
Shreyon Roy <shreyon312@gmail.com>
2023-10-10 16:51:15 -0500

    My second commit: Changed to Vite

--- FrontEnd/webpage/src/Navbar.jsx
+++ FrontEnd/webpage/src/Navbar.jsx
@@ -4,8 +4,8 @@ export default function Navbar() {
         <nav className="nav">
             <img src={UTDIcon} className="utd-icon" alt=""></img>
             <div className="navbar-caption">
-                <h1 className="navbar-cap-1">Virtual TA</h1>
-                <h1 className="navbar-cap-2">CS4349 Advanced Algorithms Design and Analysis</h1>
+                <h2 className="navbar-cap-1">Virtual TA</h2>
+                <h2 className="navbar-cap-2">CS4349 Advanced Algorithms Design and Analysis</h2>
             </div>
             
         </nav>
