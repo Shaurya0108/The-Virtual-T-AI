b2ac482734fce343b53729bed9ca4075d58394ed
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2023-10-18 13:32:18 -0500

    added ML.js function names for future development

--- Server/src/library/ML.js
+++ Server/src/library/ML.js
@@ -21,3 +21,104 @@ export async function query(Text) {
     .catch(error => console.log('error', error))
     return response;
 };
+
+// here we can define a function to see if the model is down and return a loading status to front end
+// extra if we have tme
+export async function getModelStatus(Text) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", process.env.model_key);
+    
+    var queryBody = JSON.stringify({
+        "inputs": Text.body.substring(1, Text.body.length-1)
+    });
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: queryBody,
+        redirect: 'follow'
+    };
+    
+
+    const response = await fetch(process.env.model_route,requestOptions)
+    .then(response => response.text())
+    .then( result => result)
+    .catch(error => console.log('error', error))
+    return response;
+};
+
+//Add option for chatGPT
+// can append promts infront to specify user request outside of fine tuning
+export async function queryGPT(Text) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", process.env.model_key);
+    
+    var queryBody = JSON.stringify({
+        "inputs": Text.body.substring(1, Text.body.length-1)
+    });
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: queryBody,
+        redirect: 'follow'
+    };
+    
+
+    const response = await fetch(process.env.model_route,requestOptions)
+    .then(response => response.text())
+    .then( result => result)
+    .catch(error => console.log('error', error))
+    return response;
+};
+
+//Add option for handling syllabus specific/multiple models
+export async function querySyllabus(Text) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", process.env.model_key);
+    
+    var queryBody = JSON.stringify({
+        "inputs": Text.body.substring(1, Text.body.length-1)
+    });
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: queryBody,
+        redirect: 'follow'
+    };
+    
+
+    const response = await fetch(process.env.model_route,requestOptions)
+    .then(response => response.text())
+    .then( result => result)
+    .catch(error => console.log('error', error))
+    return response;
+};
+
+export async function queryMultiModel(Text) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", process.env.model_key);
+    
+    var queryBody = JSON.stringify({
+        "inputs": Text.body.substring(1, Text.body.length-1)
+    });
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: queryBody,
+        redirect: 'follow'
+    };
+    
+
+    const response = await fetch(process.env.model_route,requestOptions)
+    .then(response => response.text())
+    .then( result => result)
+    .catch(error => console.log('error', error))
+    return response;
+};
