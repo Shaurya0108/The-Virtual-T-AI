d6ed0a3e15d4280d1138261054e1713d80e83602
Shreyon Roy <shreyon312@gmail.com>
2023-10-20 00:25:58 -0500

    Updated homepage with sessions tab

--- FrontEnd/src/components/SessionBar.jsx
+++ FrontEnd/src/components/SessionBar.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
+
 export default function SessionBar() {
+  //const [expanded, setExpanded] = useState(false);
+
+  const expand = () => {
+    document.getElementById("session-bar").style.width = "100%";
+  };
+
+  const compress = () => {
+    document.getElementById("session-bar").style.width = "0%";
+  }
+
     return (
-      <div className="SessionBar">
-        <header className="App-header">
-          SessionBar Component
-          <section >SessionBar content</section>
-        </header>
+      <div className="session-container">
+        <button className="openbtn" onClick={expand}></button>
+          <div id="session-bar" className="SessionBar">
+            <a href="javascript:void(0)" className="closebtn" onClick={compress}></a>
+            {/* <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a> */}
+            <button className="new-session">New Session</button>
+          </div>
+
       </div>
+      
     );
   }
