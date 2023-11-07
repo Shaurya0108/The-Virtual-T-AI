8b56dbdf80bd361fe1163962235f58bfe9f4ff23
Shaurya0108 <shauryadwivedi0108@gmail.com>
2023-11-07 16:02:36 -0600

    added functions for the prompt

--- FrontEnd/src/components/TextBox2.jsx
+++ FrontEnd/src/components/TextBox2.jsx
@@ -26,11 +26,11 @@ export default class ChatBox extends React.Component {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer token_here',
+                'Authorization': 'Bearer FQUL6K3DF7IPQM3FHTNHTIG09CW81QRBUIOGTF6G',
             },
             body: JSON.stringify({
                 "input": {
-                    "prompt": "[INST] <<SYS>>You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.<</SYS>>Write a Binary search tree in Python[/INST]",
+                    "prompt": "[INST] <<SYS>>You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.<</SYS>>" + prompt + "[/INST]",
                     "max_new_tokens": 500,
                     "temperature": 0.9,
                     "top_k": 50,
