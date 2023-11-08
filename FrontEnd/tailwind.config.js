76596eab7cbbc5f23f27f6453c52f720306abf3e
Shaurya0108 <shauryadwivedi0108@gmail.com>
2023-11-07 23:11:15 -0600

    more style fixes

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['PT Serif', 'Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
