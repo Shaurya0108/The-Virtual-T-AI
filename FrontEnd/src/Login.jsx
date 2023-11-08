75b81844f5c17c31463dd9372c2b68a042104d75
Shaurya0108 <shauryadwivedi0108@gmail.com>
2023-11-08 09:09:16 -0600

    Update Login.jsx

--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -29,17 +29,23 @@ export default function Login() {
         redirect: 'follow',
         credentials: 'include' //Make sure to have this line for every Request. Or else the cookie won't be included in the requests
       };
+      // const response = await fetch("http://18.189.195.246:443/auth/login", requestOptions);
       const response = fetch("http://18.189.195.246:443/auth/login", requestOptions);
+
       if (response.ok) {
         navigator('home');
       }
       else {
         alert('Invalid Credentials');
       }
+  //  }
+  //   catch (err) {
+  //     alert(err.message);
+  //   }
+  // }
 
 
   return (
-    
       <div className="Login">
         <button className="student-login">
           STUDENT LOGIN
@@ -62,7 +68,5 @@ export default function Login() {
           </button>
         </div>
       </div>
-    
-
   )
 }
