0de47fadc5325d92b90a4182eead5237078f4a77
Shreyon Roy <shreyon312@gmail.com>
2023-11-10 10:02:06 -0600

    Add settings button to Navbar

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ export default function Navbar() {
                 <h1 className="navbar-cap-1 text-4xl font-bold">The Virtual Teacher Assistant</h1>
                 <h3 className="navbar-cap-2 text-xl">Advanced Algorithms Design and Analysis</h3>
             </div>
+            <button className="settings-button"></button>
         </nav>
     );
 }
